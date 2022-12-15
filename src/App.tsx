fc8843448dd9af530974e73e375fe26f9242d5cb
Fressermenschen <alekskedalo@gmail.com>
2022-12-15 12:07:07 +0300

    Contract + Web App

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,28 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useStorageUpload, useContract, useContractWrite, useContractRead, Web3Button} from "@thirdweb-dev/react";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import "./styles/Home.css";
 
 export default function Home() {
-  return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.tsx</code>, then modify the{" "}
-          <code className="code">src/App.tsx</code> file!
-        </p>
-
-        <div className="connect">
-          <ConnectWallet />
+    const { contract } = useContract("0x58DD2f5b74662A781177E1daa0c9A2eF10a38a8B");
+    const { mutateAsync: addnewipfsrecord } = useContractWrite(contract, "addnewipfsrecord");
+    const { mutateAsync: upload } = useStorageUpload();
+    const onDrop = useCallback(
+      async (acceptedFiles: File[]) => {
+        const uris = await upload({ data: acceptedFiles });
+        console.log(uris);
+        const tx = await addnewipfsrecord(["0",uris]);
+      },
+      [upload],
+    );
+    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  
+    return (
+      <div>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <button>Upload medical record to IPFS</button>
         </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+      </div>
+    );
 }
